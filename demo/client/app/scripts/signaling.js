eb431b6dda6f2ddffe376c964f9f79fbcaded094
Egor Vinogradov <egorvinogradov@live.ru>
2016-02-03 22:51:27 -0800

    fix signaling server

--- demo/client/app/scripts/signaling.js
+++ demo/client/app/scripts/signaling.js
@@ -1,6 +1,7 @@
 angular.module('phonertcdemo')
   .factory('signaling', function (socketFactory) {
-    var socket = io.connect('http://192.168.1.100:3000/');
+    //var socket = io.connect('http://192.168.1.100:3000/');
+    var socket = io.connect('http://calm-stream-9272.herokuapp.com:80/');
     
     var socketFactory = socketFactory({
       ioSocket: socket
